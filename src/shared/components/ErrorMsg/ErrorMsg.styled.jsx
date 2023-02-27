d61076a804d4a763123f4d6b8d04a7335360fe88
supirgigi <107944673+supirgigi@users.noreply.github.com>
2023-02-27 21:52:01 +0200

    upd

--- src/shared/components/ErrorMsg/ErrorMsg.styled.jsx
+++ src/shared/components/ErrorMsg/ErrorMsg.styled.jsx
@@ -4,4 +4,5 @@ export const Text = styled.p`
   margin-top: 10px;
   color: ${props => props.theme.colors.red};
   font-size: 18px;
+  text-align: center;
 `;
--- src/shared/components/Loader/Loader.styled.jsx
+++ src/shared/components/Loader/Loader.styled.jsx
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const Text = styled.p`
   margin-top: 10px;
   font-size: 18px;
+  text-align: center;
 `;
